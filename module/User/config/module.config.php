9b5dbb83c9cab17b23418c54fbf78804624f47b0
Deys <deysalex@gmail.com>
2015-06-07 16:42:55 +0600

    Added layout for User module

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -60,6 +60,9 @@ return array(
         ),
     ),
     'view_manager' => array(
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+        ),
         'template_path_stack' => array(
             'user' => __DIR__ . '/../view',
         ),
